11753b50634c01bea9421db9c4f0ebb124997454
Guido Marzocchini <guido.marzocchini@gmail.com>
2023-08-10 19:48:03 -0300

    error a arreglar de numColumns

--- src/screens/characterScreen.js
+++ src/screens/characterScreen.js
@@ -1,38 +1,43 @@
 import { useState, useEffect } from 'react';
-import { View, FlatList, Text } from 'react-native';
+import { View, FlatList, Text, Image } from 'react-native';
 import { viewStyles } from '../styles/viewStyle';
 import { textStyles } from '../styles/textStyle';
 
 const apiURL = 'https://rickandmortyapi.com/api/character/';
 
-const renderList = ({ item }) => {
+const RenderList = ({ item }) => {
     return (
       <View style={viewStyles.container}>
-        <Text style={textStyles.text}>{item.id} {item.name}</Text>
+        <Text style={textStyles.text}>
+          {item.id} {item.name}
+        </Text>
       </View>
     );
   };
 
-
 export function CharacterScreen(){
     
-    const [data, setData] = useState([]);
-    
-    useEffect(() => {
+    const [data, setData] = useState([]);  
+    try {
+      useEffect(() => {
         fetch(apiURL,{
             method: 'GET',
         })
         .then(response => response.json())
         .then(json => {setData(json.results)})
-    }, [])
-
+      }, [])
+    }  catch(error){
+      console.log(error);
+    }
+    
 
     return (
         <View style={ viewStyles.container }>
-                <FlatList 
-                    data={data}
-                    renderItem={renderList}
-                />
+          <FlatList 
+            numColumns={2}
+            data={data}
+            renderItem={({ item }) =>  <RenderList item={ item }/>} 
+          />
         </View>
     )
 }
